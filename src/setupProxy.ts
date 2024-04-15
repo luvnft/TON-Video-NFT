25b184dd3c8c6fdf784a3b387cbfa9d30fa58e85
kartik <kartikb.5578@gmail.com>
2024-04-15 11:20:25 +0530

    cors error solved

---
+++ src/setupProxy.ts
@@ -0,0 +1,13 @@
+// src/setupProxy.ts
+import { createProxyMiddleware } from 'http-proxy-middleware';
+import {Express} from "express" // You may need to install the types for Express if not already installed
+
+export default function setupProxy(app: Express) {
+  app.use(
+    '/api',
+    createProxyMiddleware({
+      target: 'https://api.pinata.cloud',
+      changeOrigin: true,
+    })
+  );
+}
