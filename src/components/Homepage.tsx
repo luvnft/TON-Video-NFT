ac44fceb34c93e34a3ebb48c55bf1845af8d60ed
kartik <kartikb.5578@gmail.com>
2024-04-15 11:55:49 +0530

    mint button removed

--- src/components/Homepage.tsx
+++ src/components/Homepage.tsx
@@ -19,8 +19,8 @@ function Homepage() {
               <TonConnectButton/>
             </div>
            
-            <Link to="/create">
-            <button type="button" className="text-white mt-6 bg-gradient-to-r from-purple-500 to-pink-500 hover:bg-gradient-to-l focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Mint NFT</button>
+            <Link to="/all-nft">
+            <button type="button" className="text-white mt-6 bg-gradient-to-r from-purple-500 to-pink-500 hover:bg-gradient-to-l focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">All NFT</button>
             </Link>
         </div>
         {/* <div className=''>
