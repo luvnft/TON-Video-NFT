0d4c685eb7ccf97bba815ad6582cabfd65865d2b
The Wizard of Hahz <info@luvnft.com>
2024-09-22 18:14:25 -0400

    updated x

--- src/components/Homepage.tsx
+++ src/components/Homepage.tsx
@@ -11,7 +11,7 @@ function Homepage() {
         
         <div className='mb-16 max-sm:text-center '>
             <h1 className='text-6xl font-semibold'>PPV is the Telegram OF<br></br><span className='font-thin text-sky-400'>with no middleman fee. </span></h1>
-            <p className='pt-8 text-xl font-thin'> Earn 💯 in Telegram $TON currency 24/7/365 <br></br>by selling short (Vids less than 60 secs) as PPV. <br></br>Join <a href="https://t.me/ppvgem" target="_blank" style={{fontWeight: "bold", textDecoration: "underline"}}>@ppvgem</a> Telegram to promote your PPV.</p>
+            <p className='pt-8 text-xl font-thin'> Earn 💯 in Telegram $TON currency 24/7/365 <br></br>by selling short (Vids less than 60 secs) as PPV. <br></br>Join <a href="https://t.me/ppvgem" target="_blank" style={{fontWeight: "bold", textDecoration: "underline"}}>@ppvgem</a> Telegram to promote your PPV.</p><br></br>Follow <a href="https://x.com/ppvgem" target="_blank" style={{fontWeight: "bold", textDecoration: "underline"}}>@ppvgem</a> on X.
             <br></br>
             <h2>Connect your Telegram wallet</h2>
             <br></br>
